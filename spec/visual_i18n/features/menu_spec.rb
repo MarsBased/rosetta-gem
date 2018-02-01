316ee9fcb5b433ba00b5c5c83e618d003f2ec235
Juan Artero <juan.artero@gmail.com>
2018-02-01 17:58:19 +0100

    Rename button

--- spec/visual_i18n/features/menu_spec.rb
+++ spec/visual_i18n/features/menu_spec.rb
@@ -2,7 +2,7 @@ feature 'Show menu' do
   before { visit root_path }
 
   scenario 'with page translations', js: true do
-    find('.open-translations-btn').click
+    find('.open-phrases-menu-btn').click
 
     expect(page).to have_selector '.phrases-menu.open'
     expect(page).to have_content 'Hello world!!'
