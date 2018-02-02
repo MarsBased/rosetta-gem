f1afdd689de932ac48e59d9c567cbe97d38d62af
Juan Artero <juan.artero@gmail.com>
2018-02-02 16:16:06 +0100

    Remove OneSky References

--- lib/visual_i18n.rb
+++ lib/visual_i18n.rb
@@ -7,7 +7,6 @@ require 'visual_i18n/i18n'
 require 'visual_i18n/phrase'
 require 'visual_i18n/repository'
 require 'visual_i18n/repositories/local'
-require 'visual_i18n/repositories/onesky'
 require 'request_store'
 require 'font-awesome-rails'
 
--- spec/visual_i18n/repository/onesky_spec.rb
+++
@@ -1,7 +0,0 @@
-module VisualI18n
-  module Repositories
-    RSpec.describe OneSky do
-      describe '#initialize'
-    end
-  end
-end
