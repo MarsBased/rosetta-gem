df6c8050a43734162c43991992270c4058cca1de
Juan Artero <juan.artero@marsbased.com>
2018-05-17 10:55:34 +0200

    Fix performcance with local repository, close #9

--- lib/visual_i18n/i18n.rb
+++ lib/visual_i18n/i18n.rb
@@ -12,27 +12,12 @@ module I18n
       if VisualI18n.store_phrases?
         keys = normalize_keys('', args.dig(0), args.dig(1, :scope))
         code = keys.join('.')
-        if exists?(code, config.locale)
-          filename = load_path.flatten.find { |filename| file_has_phrase?(filename, keys) }
-          VisualI18n.add_phrase(keys: keys,
-                                text: output,
-                                file_path: Pathname.new(filename).relative_path_from(Rails.root))
-        end
+
+        VisualI18n.add_phrase(keys: keys, text: output) if exists?(code, config.locale)
       end
 
       output
     end
-
-    def file_has_phrase?(filename, keys)
-      type = File.extname(filename).tr('.', '').downcase
-      return unless type == 'yml'
-      data = backend.send(:load_yml, filename)
-      unless data.is_a?(Hash)
-        fail InvalidLocaleData.new(filename, 'expects it to return a hash, but does not')
-      end
-
-      data.dig(config.locale.to_s, *keys.map(&:to_s))
-   end
 
   end
 end
--- lib/visual_i18n/repositories/local.rb
+++ lib/visual_i18n/repositories/local.rb
@@ -7,19 +7,27 @@ module VisualI18n
         path && Pathname.new(path).relative_path_from(Rails.root)
       end
 
-      # rubocop:disable Style/Send
       def self.file_has_phrase?(filename, keys)
         type = File.extname(filename).tr('.', '').downcase
         return unless type == 'yml'
-        data = I18n.backend.send(:load_yml, filename)
-        unless data.is_a?(Hash)
+
+        unless file_translations(filename).is_a?(Hash)
           fail I18n::InvalidLocaleData.new(filename,
                                            'expects it to return a hash, but does not')
         end
 
-        data.dig(I18n.config.locale.to_s, *keys.map(&:to_s))
+        file_translations(filename).dig(I18n.config.locale.to_s, *keys.map(&:to_s))
+      end
+
+      # rubocop:disable Style/Send
+      def self.file_translations(filename)
+        translations[filename] ||= I18n.backend.send(:load_yml, filename)
       end
       # rubocop:enable Style/Send
+
+      def self.translations
+        @translations ||= {}
+      end
 
     end
   end
