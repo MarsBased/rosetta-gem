929775993e4180ff31a663d324c9e4d488b36c90
Oriol Collell <oriol.collell.bcn@gmail.com>
2018-05-18 12:53:01 +0200

    Add rake task to install an initializer

---
+++ lib/generators/rosetta/install_generator.rb
@@ -0,0 +1,21 @@
+require 'rails/generators/base'
+
+module Rosetta
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+
+      source_root File.expand_path('../../templates', __FILE__)
+
+      desc 'Creates a Rosetta initializer'
+
+      def copy_initializer
+        template 'rosetta.rb', 'config/initializers/rosetta.rb'
+      end
+
+      def rails_4?
+        Rails::VERSION::MAJOR == 4
+      end
+
+    end
+  end
+end
