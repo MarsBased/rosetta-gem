32481ec18027dfca65ae52e15837ca1cad7a9122
Juan Artero <juan.artero@marsbased.com>
2018-05-18 14:03:55 +0200

    Add new design, copy changes from #34

--- lib/rosetta/engine.rb
+++ lib/rosetta/engine.rb
@@ -4,7 +4,8 @@ module Rosetta
     isolate_namespace Rosetta
 
     initializer :assets do |_config|
-      Rails.application.config.assets.precompile += %w[rosetta/language_icon.svg]
+      Rails.application.config.assets.precompile += %w[rosetta/language_icon.svg
+                                                       rosetta/close.svg]
     end
 
   end
