bc2797a00c9abbd2d431825f73d19015dd452e3d
Oriol Collell <oriol.collell.bcn@gmail.com>
2018-10-30 09:14:07 +0100

    Add search to phrases page (#61)

--- app/assets/javascripts/rosetta/application.js
+++ app/assets/javascripts/rosetta/application.js
@@ -1,10 +1,39 @@
-//= require ./vue.min
+//= require ./vendor/vue.min
+//= require ./vendor/lunr
 
 function initPhrasesList(selector, phrases) {
   new Vue({
     el: selector,
     data: {
-      phrases: phrases
+      phrases: phrases,
+      query: ''
+    },
+    computed: {
+      searchIndex: function() {
+        var that = this;
+
+        return lunr(function () {
+          this.ref('code');
+          this.field('text');
+
+          that.phrases.forEach(function (phrase) {
+            this.add(phrase)
+          }, this);
+        });
+      },
+      filteredPhrases: function () {
+        if (this.query) {
+          var results = this.searchIndex.search(this.query);
+
+          return this.phrases.filter(function (phrase) {
+            return results.some(function (result) {
+              return result.ref === phrase.code;
+            });
+          });
+        } else {
+          return this.phrases;
+        }
+      }
     }
   });
 }
