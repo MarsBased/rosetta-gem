2f5ed3e7d4400df5a0a90ef0a46311a695262d1d
Juan Artero <juan.artero@marsbased.com>
2018-11-06 12:06:41 +0100

    Fix dummy initializer

--- spec/dummy/config/initializers/rosetta.rb
+++ spec/dummy/config/initializers/rosetta.rb
@@ -1,24 +1,25 @@
+# frozen_string_literal: true
+
 require 'i18n/backend/active_record'
 
 # Use this hook to configure the repository and other Rosetta options.
-Rosetta.setup do |_config|
+Rosetta.setup do |config|
   # Uncomment the following lines to use the OneSky repository.
   # config.repository = Rosetta::Repositories::Onesky
   # config.onesky.project_id = ENV['ONESKY_PROJECT_ID']
   # config.onesky.subdomain = ENV['ONESKY_SUBDOMAIN']
 
   # Uncomment the following lines to use the Local repository.
-  # config.repository = Rosetta::Repositories::Local
-end
+  config.repository = Rosetta::Repositories::Database.new
+  Translation = I18n::Backend::ActiveRecord::Translation
 
-Translation = I18n::Backend::ActiveRecord::Translation
+  if Translation.table_exists?
+    I18n.backend = I18n::Backend::ActiveRecord.new
 
-if Translation.table_exists?
-  I18n.backend = I18n::Backend::ActiveRecord.new
+    I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Memoize)
+    I18n::Backend::Simple.send(:include, I18n::Backend::Memoize)
+    I18n::Backend::Simple.send(:include, I18n::Backend::Pluralization)
 
-  I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Memoize)
-  I18n::Backend::Simple.send(:include, I18n::Backend::Memoize)
-  I18n::Backend::Simple.send(:include, I18n::Backend::Pluralization)
-
-  I18n.backend = I18n::Backend::Chain.new(I18n.backend, I18n::Backend::Simple.new)
+    I18n.backend = I18n::Backend::Chain.new(I18n.backend, I18n::Backend::Simple.new)
+  end
 end
